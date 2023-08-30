70abda7c0ff5b95863c6dca618f7f6b135296605
Abdulhafiz Gazali Ado <ahgazali108@gmail.com>
2023-08-30 15:25:00 +0000

    task 9 done

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,15 +6,20 @@
  *
  * Return: The height of the tree, or 0 if tree is NULL.
  */
+
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	size_t left_h = 0;
+	size_t right_h = 0;
 
-	if (tree == NULL)
+	if (!tree)
 		return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	if (tree->left)
+		left_h += 1 + binary_tree_height(tree->left);
 
-	return (1 + (left_height > right_height ? left_height : right_height));
+	if (tree->right)
+		right_h += 1 + binary_tree_height(tree->right);
+
+	return (left_h > right_h) ? left_h : right_h;
 }
