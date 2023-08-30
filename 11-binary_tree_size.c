fe3360dd5d27e60d589f831f6b77f229da44d28c
Abdulhafiz Gazali Ado <ahgazali108@gmail.com>
2023-08-30 17:07:32 +0000

    task 11 done

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -9,17 +9,12 @@
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t left_h = 0;
-	size_t right_h = 0;
+	size_t left_h, right_h;
 
 	if (!tree)
 		return (0);
 
-	if (tree->left)
-		left_h += 1 + binary_tree_size(tree->left);
-
-	if (tree->right)
-		right_h += 1 + binary_tree_size(tree->right);
-
-	return (left_h + right_h + 1);
+	left_h = binary_tree_size(tree->left);
+	right_h = binary_tree_size(tree->right);
+	return (right_h + left_h + 1);
 }
