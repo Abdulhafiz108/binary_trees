dafc6fb5f2648fb1dff1ac503e64d5aa81a2db06
Abdulhafiz Gazali Ado <ahgazali108@gmail.com>
2023-08-31 23:56:12 +0000

    task 18 done

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -10,7 +10,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	binary_tree_t *tmp;
 
-	if ((node == NULL) || (node->parent == NULL))
+	if (node == NULL)
 		return (NULL);
 
 	tmp = node->parent->parent->right;
